1962d3489c24235e8b18d01035fa8c8dc734e2ea
kragol <gabriel.rilling@gmail.com>
2020-08-06 18:03:47 +0200

    fixed incorrect scaling bug in embedding(cfg::ShiMalikLaplacian, L::NormalizedLaplacian)

--- src/Embedding.jl
+++ src/Embedding.jl
@@ -314,7 +314,7 @@ function embedding(cfg::ShiMalikLaplacian, L::NormalizedLaplacian)
     (vals, V) = LightGraphs.eigs(sparse(L), nev=min(cfg.nev + 10, size(L, 1)), which = SR(), restarts=5000)
     idxs = findall(real(vals) .> 0.0000001)
     idxs = idxs[1:min(length(idxs), cfg.nev)]
-    V = spdiagm(0 => L.A.A.D.^(1 / 2)) * real(V[:,idxs])
+    V = spdiagm(0 => L.A.A.D.^(-1 / 2)) * real(V[:,idxs])
     if cfg.normalize
         return normalize_rows(V)
     else
